80ae238d0e5c006ea67288e0c0704e5a3d07876e
Easin Arafat <arafatmrx@gmail.com>
2021-10-31 17:14:51 +0600

    footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import img from "../../img/logo.png";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        <h2 className="text-danger">Health First Medicine </h2>
+        <p className="pp">
+          Majority of the drug shops studied are run by salespersons who have
+          informal ‘training’ through apprenticeship. Visiting drug shops
+          without a prescription, and dispensing without counseling unless
+          pro-actively sought by the client, was very common. The existing
+          process is discouraging for the shop owners to seek license, and the
+          shop inspection visits are irregular, unstructured and punitive. These
+          facts should be considered while designing an accredited model of drug
+          shop for Bangladesh.
+        </p>
+        <h5>Copyright 2021 &copy; Easin Arafat</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
