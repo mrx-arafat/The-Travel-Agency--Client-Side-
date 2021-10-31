c389cbfe486578d4c1634605cabb82bde4b26327
Easin Arafat <arafatmrx@gmail.com>
2021-11-01 04:33:34 +0600

    firebase

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyChY6woWkYDH4UDpM2xJG2IgiInVvDvDzY",
+  authDomain: "travel-agency-1b112.firebaseapp.com",
+  projectId: "travel-agency-1b112",
+  storageBucket: "travel-agency-1b112.appspot.com",
+  messagingSenderId: "274925160290",
+  appId: "1:274925160290:web:c0179e74bb17b737380e3b",
+};
+export default firebaseConfig;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
     <div>
       <h2>please register</h2>
+
+      <form>
+        <input type="text" />
+        <br />
+        <input type="text" />
+        <br />
+        <input type="submit" value="register" />
+      </form>
+
+      <Link to="/login">Go to Login</Link>
     </div>
   );
 };
