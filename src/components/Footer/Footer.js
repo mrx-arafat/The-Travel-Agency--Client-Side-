8010cdec066fdaafa22be1844ffd15569fe45f88
Easin Arafat <arafatmrx@gmail.com>
2021-10-31 20:20:08 +0600

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,14 +9,10 @@ const Footer = () => {
         <h2 className=" text-danger">The Travel </h2>
         <img className="logo" src={logo} alt="" />
         <p className="pp">
-          Majority of the drug shops studied are run by salespersons who have
-          informal ‘training’ through apprenticeship. Visiting drug shops
-          without a prescription, and dispensing without counseling unless
-          pro-actively sought by the client, was very common. The existing
-          process is discouraging for the shop owners to seek license, and the
-          shop inspection visits are irregular, unstructured and punitive. These
-          facts should be considered while designing an accredited model of drug
-          shop for Bangladesh.
+          Show off your brand’s personality with a custom travel agency logo
+          designed just for you by a professional designer. Need ideas? We’ve
+          collected some amazing examples of travel agency logos from our global
+          community of designers.
         </p>
         <h5>Copyright 2021 &copy; Easin Arafat</h5>
       </div>
