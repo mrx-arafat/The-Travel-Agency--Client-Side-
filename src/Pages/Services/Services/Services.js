fb7990230354647245f2feaff3039d07a9a71641
Easin Arafat <arafatmrx@gmail.com>
2021-11-17 00:59:45 +0600

    Taking Order Successfully

--- src/Pages/Services/Services/Services.js
+++ src/Pages/Services/Services/Services.js
@@ -1,9 +1,11 @@
+import { Alert } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import LoadServices from "../LoadServices/LoadServices";
 import "./Services.css";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [bookingSuccess, setBookingSuccess] = useState(false);
   useEffect(() => {
     fetch("http://localhost:5000/services")
       .then((res) => res.json())
@@ -12,9 +14,15 @@ const Services = () => {
   return (
     <div>
       <h1 className="p-4 text-danger fw-bold">We Are Offering</h1>
+      {bookingSuccess?.email && (
+        <Alert severity="success">Pre Ordered Successfully!</Alert>
+      )}
       <div className="service-container mb-5">
-        {services.map((sv) => (
-          <LoadServices sv={sv}></LoadServices>
+        {services.map((service) => (
+          <LoadServices
+            service={service}
+            setBookingSuccess={setBookingSuccess}
+          ></LoadServices>
         ))}
       </div>
     </div>
